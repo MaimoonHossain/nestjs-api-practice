d6e0f9dac14b54692d456ece8c1f0ed413849d22
MaimoonHossain <ashikasitosh740@gmail.com>
2025-11-19 17:43:36 +0600

    new apis integrated and backend re configured and a lot of new things added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,48 @@
-import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
+import { Injectable } from '@nestjs/common';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { msg: 'I am signed up' };
+
+  async signup(dto: AuthDto) {
+    const hash = await argon2.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    return user;
   }
 
-  signin() {
-    return { msg: 'I am signed in' };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) {
+      return {
+        statusCode: 403,
+        message: 'Invalid credentials',
+      };
+    }
+
+    const pwMatches = await argon2.verify(user.hash, dto.password);
+
+    if (!pwMatches) {
+      return {
+        statusCode: 403,
+        message: 'Invalid credentials',
+      };
+    }
+
+    return user;
   }
 }
