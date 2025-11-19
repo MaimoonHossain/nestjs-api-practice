bcc1a03eabdf500dafdc3f945e1ba6aedd73cf95
MaimoonHossain <ashikasitosh740@gmail.com>
2025-11-19 17:49:21 +0600

    validation fixed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,24 @@
+import { Injectable, ForbiddenException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon2 from 'argon2';
-import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
+    const checkIfExists = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (checkIfExists) {
+      throw new ForbiddenException('User already exists');
+    }
+    // Hash password
     const hash = await argon2.hash(dto.password);
 
+    // Create user
     const user = await this.prisma.user.create({
       data: {
         email: dto.email,
@@ -17,32 +26,30 @@ export class AuthService {
       },
     });
 
-    return user;
+    // Remove hash before sending response
+    const { hash: _, ...safeUser } = user;
+
+    return safeUser;
   }
 
   async signin(dto: AuthDto) {
     const user = await this.prisma.user.findUnique({
-      where: {
-        email: dto.email,
-      },
+      where: { email: dto.email },
     });
 
     if (!user) {
-      return {
-        statusCode: 403,
-        message: 'Invalid credentials',
-      };
+      throw new ForbiddenException('Invalid credentials');
     }
 
     const pwMatches = await argon2.verify(user.hash, dto.password);
 
     if (!pwMatches) {
-      return {
-        statusCode: 403,
-        message: 'Invalid credentials',
-      };
+      throw new ForbiddenException('Invalid credentials');
     }
 
-    return user;
+    // Remove hash before sending response
+    const { hash: _, ...safeUser } = user;
+
+    return safeUser;
   }
 }
